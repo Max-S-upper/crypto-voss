3330a83b385116146f4ef7895f04c0fb46df3e9d
Artur Lavrinovich <hey@Arturs-MacBook-Pro.local>
2021-03-21 02:34:29 +0200

    hey

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class HomeController extends Controller
 {
     public function index() {
-        $client = new GuzzleHttp\Client();
+        $client = new \GuzzleHttp\Client();
         $btcRate = $client->get(env('COIN_API_BASE_URL') . 'exchangerate/BTC/USDT', [
             'apiKey' => env('COIN_API_KEY')
         ]);
