c031e5b5a6757a10c2356d7cd009df77b5ab3d6e
Maxim Sokolskyi <maksim.sokolskyi@nixsolutions.com>
2021-03-21 02:27:44 +0200

    a

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index() {
+        $client = new GuzzleHttp\Client();
+        $btcRate = $client->get(env('COIN_API_BASE_URL') . 'exchangerate/BTC/USDT', [
+            'apiKey' => env('COIN_API_KEY')
+        ]);
+
+        return view('welcome', [
+            'btcRate' => $btcRate
+        ]);
+    }
+}
